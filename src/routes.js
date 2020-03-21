0215ab703b097ec950182770e9f5be1ec1a74b1f
Hudson11 <hudsonhdj36@gmail.com>
2020-03-21 15:54:47 -0300

    Adicionado Routa para filtragem de países

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,30 @@
+const dataset = require('./dataset/covid.json');
 const { Router } = require('express');
 
 const routes = new Router();
 
 routes.get('/allCountries', (request, response) => {
-  return response.json({ msg: 'ok' });
+  return response.json({ msg: 'ok Pedro humilde' });
 });
+
+routes.get('/allData', (req, res) => {
+  return res.json(dataset);
+})
+
+routes.get('/countries', (req, res) => {
+  const {country} = req.query
+  
+  const data = dataset.filter( (item) => 
+    country.toLowerCase()
+      .normalize('NFD')
+      .replace(/[\u0300-\u036f]/g, '')
+        ==
+    item.country_other.toLowerCase()
+      .normalize('NFD')
+      .replace(/[\u0300-\u036f]/g, '')
+  )
+
+  return res.json(data)
+})
 
 module.exports = routes;
