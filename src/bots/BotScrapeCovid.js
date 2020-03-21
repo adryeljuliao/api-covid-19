aadf2206121ea9e71d3cceb9fc2c021df4c3331a
Adryel Julião <adryel.juliao1@gmail.com>
2020-03-21 00:10:25 -0300

    Add tratament data

--- src/bots/BotScrapeCovid.js
+++ src/bots/BotScrapeCovid.js
@@ -2,22 +2,41 @@ const cheerio = require('cheerio');
 const axios = require('axios');
 const request = require('request');
 
-request(
-  'https://www.worldometers.info/coronavirus/',
-  (error, response, html) => {
-    if (!error && response.statusCode == 200) {
-      console.log(html);
-    }
-  }
-);
+// request(
+//   'https://www.worldometers.info/coronavirus/',
+//   (error, response, html) => {
+//     if (!error && response.statusCode == 200) {
+//       console.log(html);
+//     }
+//   }
+// );
 
-// axios.get('https://www.worldometers.info/coronavirus/').then(response => {
-// const $ = cheerio.load(response.data);
-// const data = [];
-// $('#main_table_countries_today thead th').each(function() {
-//   data.push($(this).text());
-//   console.log($(this).text());
-// });
-// console.log(data);
-//   console.log(response.data);
-// });
+axios.get('https://www.worldometers.info/coronavirus/').then(response => {
+  const $ = cheerio.load(response.data);
+  const data = [];
+  const labelsHead = [];
+  $('#main_table_countries_today thead th').each(function() {
+    labelsHead.push(
+      $(this)
+        .text()
+        .toLowerCase()
+        .replace(',', '')
+        .replace(/( |\/){1,}/g, '')
+        .trim()
+    );
+  });
+
+  $('#main_table_countries_today tbody tr').each(function(indexRow) {
+    let dataCountry = {};
+    $(this)
+      .find('td')
+      .each(function(indexColumn) {
+        key = labelsHead[indexColumn];
+        dataCountry[key] = $(this)
+          .text()
+          .trim();
+      });
+    data.push(dataCountry);
+  });
+  console.log(data);
+});
