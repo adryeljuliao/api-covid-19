863f7283ecc6d3b64a9cf71589ef4fa7dc3c9baf
Adryel Julião <adryel.juliao1@gmail.com>
2020-03-21 21:52:33 -0300

    Add job execution to update dataset

--- src/app.js
+++ src/app.js
@@ -1,19 +1,30 @@
 const express = require('express');
 const cors = require('cors');
+const CronJob = require('cron').CronJob;
+
 const routes = require('./routes');
+const updateDataCovid = require('./bots');
 
 class App {
   constructor() {
     this.server = express();
     this.middlewares();
     this.routes();
+    this.jobUpdateDataset();
   }
 
   middlewares() {
     this.server.use(cors());
     this.server.use(express.json());
   }
-
+  jobUpdateDataset() {
+    const job = new CronJob('0 */10 * * * *', function() {
+      const d = new Date();
+      console.log('At Ten Minutes:', d);
+      updateDataCovid();
+    });
+    job.start();
+  }
   routes() {
     this.server.use(routes);
   }
